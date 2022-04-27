3fef3f6c38d66bce4bf858d2277997399737668e
Konstantinos Petsanoukis <kostaspe01@gmail.com>
2022-04-28 01:32:18 +0300

    Add files via upload

---
+++ Calculator/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>ΑΡΙΘΜΟΜΗΧΑΝΗ</h1>
+        Αριθμός Α:
+        <asp:TextBox ID="TextBox1" runat="server" Width="54px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="+" />
+&nbsp;<asp:Button ID="btnSubtract" runat="server" OnClick="btnSubtract_Click" Text="-" />
+&nbsp;<asp:Button ID="btnMultiply" runat="server" OnClick="btnMultiply_Click" Text="x" />
+&nbsp;<asp:Button ID="btnDivide" runat="server" OnClick="btnDivide_Click" Text="/" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" Text="Καθάρισμα" />
+        <br />
+        <br />
+        Αριθμός Β: <asp:TextBox ID="TextBox2" runat="server" Width="53px"></asp:TextBox>
+        <br />
+        <br />
+        Αποτέλεσμα:
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+    </form>
+</body>
+</html>
---
+++ Calculator/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    com.dneonline.www.Calculator myWS = new com.dneonline.www.Calculator();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        int a, b, apot;
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        apot = myWS.Add(a,b);
+
+        Label1.Text = Convert.ToString(apot);
+    }
+
+    protected void btnSubtract_Click(object sender, EventArgs e)
+    {
+        int a, b, apot;
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        apot = myWS.Subtract(a, b);
+
+        Label1.Text = Convert.ToString(apot);
+    }
+
+    protected void btnMultiply_Click(object sender, EventArgs e)
+    {
+        int a, b, apot;
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        apot = myWS.Multiply(a, b);
+
+        Label1.Text = Convert.ToString(apot);
+    }
+
+    protected void btnDivide_Click(object sender, EventArgs e)
+    {
+
+        int a, b, apot;
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        if (b != 0)
+        {
+            apot = myWS.Divide(a, b);
+            Label1.Text = Convert.ToString(apot);
+        }
+        else Label1.Text = "Δεν ορίζεται!";
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = "";
+        TextBox2.Text = "";
+        Label1.Text = "";
+    }
+}
